09dc9f9ca4912acb628d78cfd698f85b5951a2df
Martin Marinov <m.marinov1733@gmail.com>
2026-02-20 23:37:36 +0200

    puppy

--- 01.Inheritance-Lab/01-single-inheritance/StartUp.cs
+++ 01.Inheritance-Lab/01-single-inheritance/StartUp.cs
@@ -4,9 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Dog dog = new Dog();
-            dog.Bark();
-            dog.Bark();
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
         }
     }
 }
