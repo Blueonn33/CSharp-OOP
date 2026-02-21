d3bef3ba406d72a1a738a05073eabddf7bf97426
Martin Marinov <m.marinov1733@gmail.com>
2026-02-21 21:09:18 +0200

    nfs hot pursuit

---
+++ 02-Inheritance-Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle(100, 200);
+                Console.WriteLine(vehicle.HorsePower);
+                Console.WriteLine(vehicle.Fuel);
+                Console.WriteLine(vehicle.FuelConsumption);
+                vehicle.Drive(100);
+                Console.WriteLine(vehicle.Fuel);
+        }
+    }
+}
