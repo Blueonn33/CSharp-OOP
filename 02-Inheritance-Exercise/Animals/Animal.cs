18fa63dec5b6cc3c5097821e2fcfd4f2c51f76ba
Martin Marinov <m.marinov1733@gmail.com>
2026-02-21 22:04:33 +0200

    animals

---
+++ 02-Inheritance-Exercise/Animals/Animal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+            }
+        }
+
+        public string Gender
+        {
+            get => gender;
+            set { gender = value; }
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}\n{Name} {Age} {Gender}";
+        }
+    }
+}
---
+++ 02-Inheritance-Exercise/Animals/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            string animalType = Console.ReadLine();
+            string[] animalInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            while (animalType != "Beast!")
+            {
+                string name = animalInput[0];
+                int age = int.Parse(animalInput[1]);
+                string gender = animalInput[2];
+
+                try
+                {
+                    Animal animal = null;
+
+                    switch (animalType)
+                    {
+                        case "Dog":
+                            animal = new Dog(name, age, gender);
+                            break;
+                        case "Cat":
+                            animal = new Cat(name, age, gender);
+                            break;
+                        case "Frog":
+                            animal = new Frog(name, age, gender);
+                            break;
+                        case "Kitten":
+                            animal = new Kitten(name, age);
+                            break;
+                        case "Tomcat":
+                            animal = new Tomcat(name, age);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid input!");
+                            break;
+                    }
+
+                    animals.Add(animal);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                animalType = Console.ReadLine();
+                animalInput = Console.ReadLine().Split();
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+                Console.WriteLine(animal.ProduceSound());
+            }
+        }
+    }
+}
