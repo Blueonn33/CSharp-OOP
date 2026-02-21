f2ccadc1f512b13bbc71092ad3e4ba99149abfea
Martin Marinov <m.marinov1733@gmail.com>
2026-02-21 23:27:44 +0200

    players and monsters

---
+++ 02-Inheritance-Exercise/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private string username;
+        private int level;
+
+        public string Username
+        {
+            get; set;
+        }
+        public int Level
+        {
+            get; set;
+        }
+
+        public Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {Username} Level: {Level}";
+        }
+    }
+}
