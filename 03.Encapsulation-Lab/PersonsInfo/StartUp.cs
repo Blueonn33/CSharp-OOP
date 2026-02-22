da0ffdfeb6467697178e9fbdbc0eb33cf6546aa1
Martin Marinov <m.marinov1733@gmail.com>
2026-02-22 11:11:54 +0200

    person

---
+++ 03.Encapsulation-Lab/PersonsInfo/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Channels;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var firstName = input[0];
+                var lastName = input[1];
+                var age = int.Parse(input[2]);
+
+                Person person = new Person(firstName, lastName, age);
+                people.Add(person);
+            }
+
+            people.OrderBy(p => p.FirstName)
+                  .ThenBy(p => p.Age)
+                  .ToList()
+                  .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
