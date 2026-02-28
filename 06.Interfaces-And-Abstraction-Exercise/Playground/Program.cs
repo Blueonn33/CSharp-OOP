9d06118b6c8d582cafc002fb0cbcc11c4a837939
Martin Marinov <m.marinov1733@gmail.com>
2026-02-28 19:51:48 +0200

    person

---
+++ 06.Interfaces-And-Abstraction-Exercise/Playground/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Net.Http.Headers;
+
+namespace Playground
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor doctor = new Doctor();
+            IWorker teacher = new Teacher();
+
+            List<IWorker> workers = new();
+            workers.Add(doctor);
+            workers.Add(teacher);
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            doctor.Diagnose();
+
+            List<IEmployee> employees = new();
+            employees.Add(doctor);
+        }
+    }
+
+    public interface IWorker
+    {
+        void Work();
+    }
+
+    public interface IEmployee
+    {
+        void GetSalary();
+        void PayTaxes();
+    }
+
+    public class Doctor : IWorker, IEmployee
+    {
+        public void Work()
+        {
+            Console.WriteLine("Cure people");
+        }
+
+        public void Diagnose()
+        {
+            Console.WriteLine("Diagnoses sickness");
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PayTaxes()
+        {
+            Console.WriteLine("Paying :(");
+        }
+    }
+
+    public class Teacher : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Teach people");
+        }
+
+        public void CreateTest()
+        {
+            Console.WriteLine("Creates a test");
+        }
+    }
+
+    public class Manager : IEmployee
+    {
+        public void GetSalary()
+        {
+            Console.WriteLine(4323);
+        }
+
+        public void PayTaxes()
+        {
+            Console.WriteLine(54534);
+        }
+    }
+}
