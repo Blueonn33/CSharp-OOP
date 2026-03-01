907029ef6c594e5111f01cf3c5adbf015fed6617
Martin Marinov <m.marinov1733@gmail.com>
2026-03-01 16:28:17 +0200

    name

---
+++ 06.Interfaces-And-Abstraction-Exercise/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using ExplicitInterfaces.Models;
+using ExplicitInterfaces.Models.Interfaces;
+
+namespace ExplicitInterfaces
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Citizen> citizens = new();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = tokens[0];
+                string country = tokens[1];
+                int age = int.Parse(tokens[2]);
+
+                Citizen citizen = new(name, country, age);
+                citizens.Add(citizen);
+            }
+
+            foreach (Citizen citizen in citizens)
+            {
+                Console.WriteLine(((IPerson)citizen).GetName());
+                Console.WriteLine(((IResident)citizen).GetName());
+            }
+        }
+    }
+}
