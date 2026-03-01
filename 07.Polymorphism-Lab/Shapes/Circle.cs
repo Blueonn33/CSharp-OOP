7f57168bc7988b854ef0abc05c80addeb892e638
Martin Marinov <m.marinov1733@gmail.com>
2026-03-01 21:16:27 +0200

    shapes

---
+++ 07.Polymorphism-Lab/Shapes/Circle.cs
@@ -0,0 +1,27 @@
+﻿namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double Radius
+        {
+            get; private set;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public override string Draw()
+        {
+            return $"Drawing Circle";
+        }
+    }
+}
