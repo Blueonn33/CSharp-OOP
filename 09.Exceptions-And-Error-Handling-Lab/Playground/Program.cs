31375ba87b5aa9d7b86ac6127c94d266d4e8dcce
Martin Marinov <m.marinov1733@gmail.com>
2026-03-07 23:30:02 +0200

    exceptions

---
+++ 09.Exceptions-And-Error-Handling-Lab/Playground/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Playground
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            try
+            {
+                var number = int.Parse(input);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+
+            Console.WriteLine("Ok");
+        }
+    }
+}
