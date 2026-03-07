e798bd3d647ec14ffb6b0c0f2198a17b3d7c5db9
Martin Marinov <m.marinov1733@gmail.com>
2026-03-07 21:02:02 +0200

    check type

--- 08.Polymorphism-Exercise/Raiding/StartUp.cs
+++ 08.Polymorphism-Exercise/Raiding/StartUp.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            List<IHero> heroes = new List<IHero>(capacity: n);
+
+            while (heroes.Count < n)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+
+                IHero? currentHero = null;
+
+                if (type == "Druid")
+                {
+                    currentHero = new Druid { Name = name };
+                }
+                else if (type == "Paladin")
+                {
+                    currentHero = new Paladin { Name = name };
+                }
+                else if (type == "Rogue")
+                {
+                    currentHero = new Rogue { Name = name };
+                }
+                else if (type == "Warrior")
+                {
+                    currentHero = new Warrior { Name = name };
+                }
+                else
+                {
+                    Console.WriteLine("Invalid hero!");
+                }
+
+                if (currentHero is not null)
+                {
+                    heroes.Add(currentHero);
+                }
+
+
+            }
         }
     }
 }
