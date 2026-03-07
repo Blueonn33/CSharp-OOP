27dcd1ce2337e10dfc5e3bd42464f6d15c5b454c
Martin Marinov <m.marinov1733@gmail.com>
2026-03-07 21:26:58 +0200

    dictionary heroes

--- 08.Polymorphism-Exercise/Raiding/StartUp.cs
+++ 08.Polymorphism-Exercise/Raiding/StartUp.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            Dictionary<string, Func<string, IHero>> factory = new Dictionary<string, Func<string, IHero>>
+            {
+                ["Druid"] = (name) => new Druid { Name = name },
+                ["Paladin"] = (name) => new Paladin { Name = name },
+                ["Rogue"] = (name) => new Rogue { Name = name },
+                ["Warrior"] = (name) => new Warrior { Name = name },
+            };
+
             int n = int.Parse(Console.ReadLine());
             List<IHero> heroes = new List<IHero>(capacity: n);
 
