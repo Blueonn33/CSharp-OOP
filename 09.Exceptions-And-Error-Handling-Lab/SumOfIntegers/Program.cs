c8b15fcb5766c8ea32809b6f6e2f564a3b49113c
Martin Marinov <m.marinov1733@gmail.com>
2026-03-08 19:03:37 +0200

    sum

---
+++ 09.Exceptions-And-Error-Handling-Lab/SumOfIntegers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SumOfIntegers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = Console.ReadLine()
+                .Split(" ");
+
+            int sum = 0;
+
+            foreach (var item in data)
+            {
+                try
+                {
+                    var element = int.Parse(item);
+                    sum += element;
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine($"The element '{item}' is out of range!");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"The element '{item}' is in wrong format!");
+                }
+
+                Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
