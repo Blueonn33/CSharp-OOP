145e930ca45bc41ecbebadfe0f34f022034dcf05
Martin Marinov <m.marinov1733@gmail.com>
2026-03-08 18:20:27 +0200

    nums

---
+++ 09.Exceptions-And-Error-Handling-Lab/EnterNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿var allNumbers = new List<int>();
+var startNumber = 1;
+
+while (true)
+{
+    try
+    {
+        var number = ReadNumber(startNumber, 100);
+        allNumbers.Add(number);
+        startNumber = number;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid Number!");
+        continue;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    if (allNumbers.Count == 10)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(string.Join(", ", allNumbers));
+
+static int ReadNumber(int start, int end)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number <= start || number >= end)
+    {
+        throw new Exception($"Your number is not in range {start} - {end}!");
+    }
+
+    return number;
+}
+
