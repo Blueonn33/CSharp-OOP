1b96b83f79dfd4029582fc6e352015f483c4957c
Martin Marinov <m.marinov1733@gmail.com>
2026-03-09 11:35:02 +0200

    playground

---
+++ DebuggingTechniques/Playground/Program.cs
@@ -0,0 +1,32 @@
+﻿List<int> numbers = ReadNumbers();
+
+//Console.WriteLine(string.Join(", ", ManipulateNumbers(numbers)));
+PrintNumberSquare(numbers);
+
+static List<int> ManipulateNumbers(List<int> numbers)
+{
+    var result = new List<int>();
+
+    foreach (var number in numbers)
+    {
+        result.Add(number * number);
+    }
+
+    return result;
+}
+
+static List<int> ReadNumbers()
+{
+    return Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList();
+}
+
+static void PrintNumberSquare(List<int> numbers)
+{
+    foreach (var number in numbers)
+    {
+        Console.WriteLine(number * number);
+    }
+}
