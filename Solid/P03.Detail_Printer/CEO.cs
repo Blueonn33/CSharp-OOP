948030dda499f31448138c528c6b30afe03d8de1
Martin Marinov <m.marinov1733@gmail.com>
2026-03-14 20:29:18 +0200

    printer

---
+++ Solid/P03.Detail_Printer/CEO.cs
@@ -0,0 +1,21 @@
+﻿using P03.DetailPrinter;
+using System;
+using System.Collections.Generic;
+
+namespace P03.Detail_Printer
+{
+    public class CEO : Manager
+    {
+        private string financeInfo;
+
+        public CEO(string name, ICollection<string> documents, string financeInfo) : base(name, documents)
+        {
+            this.financeInfo = financeInfo;
+        }
+
+        public override string GetDetails()
+        {
+            return string.Join(Environment.NewLine, base.GetDetails(), this.Documents, this.financeInfo);
+        }
+    }
+}
--- Solid/P03.Detail_Printer/Manager.cs
+++ Solid/P03.Detail_Printer/Manager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace P03.DetailPrinter
 {
@@ -9,6 +10,14 @@ namespace P03.DetailPrinter
             this.Documents = new List<string>(documents);
         }
 
-        public IReadOnlyCollection<string> Documents { get; set; }
+        public IReadOnlyCollection<string> Documents
+        {
+            get; set;
+        }
+
+        public override string GetDetails()
+        {
+            return string.Join(Environment.NewLine, base.GetDetails(), this.Documents);
+        }
     }
 }
