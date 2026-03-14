cbc281d6a473e8b52d6f28b55dc4aee4492b0028
Martin Marinov <m.marinov1733@gmail.com>
2026-03-14 20:06:56 +0200

    prop, params

---
+++ 10.Solid/Playground/DependencyInjection/ParameterGreeter.cs
@@ -0,0 +1,23 @@
+﻿namespace Playground.DependencyInjection
+{
+    public class ParameterGreeter
+    {
+        public void Greet(ICurrentTime currentTime)
+        {
+            var timeNow = currentTime.Now;
+
+            if (timeNow.Hour < 12)
+            {
+                Console.WriteLine("Good morning");
+            }
+            else if (timeNow.Hour >= 19)
+            {
+                Console.WriteLine("Good evening");
+            }
+            else
+            {
+                Console.WriteLine("Good afternoon");
+            }
+        }
+    }
+}
---
+++ 10.Solid/Playground/DependencyInjection/PropertyGreeter.cs
@@ -0,0 +1,33 @@
+﻿namespace Playground.DependencyInjection
+{
+    public class PropertyGreeter
+    {
+        private ICurrentTime currentTime;
+
+        public ICurrentTime CurrentTime
+        {
+            set
+            {
+                this.currentTime = value;
+            }
+        }
+
+        public void Greet()
+        {
+            var timeNow = this.currentTime.Now;
+
+            if (timeNow.Hour < 12)
+            {
+                Console.WriteLine("Good morning");
+            }
+            else if (timeNow.Hour >= 19)
+            {
+                Console.WriteLine("Good evening");
+            }
+            else
+            {
+                Console.WriteLine("Good afternoon");
+            }
+        }
+    }
+}
