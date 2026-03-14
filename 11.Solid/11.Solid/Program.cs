a08ae5146e1649c6b8947a0b811d9054d4a5a02a
Martin Marinov <m.marinov1733@gmail.com>
2026-03-14 21:59:11 +0200

    дебели интерфейси

---
+++ 11.Solid/11.Solid/Program.cs
@@ -0,0 +1,54 @@
+﻿public interface IPerson
+{
+    string Job
+    {
+        get;
+    }
+}
+
+public interface ILivingCreature
+{
+    string Name
+    {
+        get;
+    }
+    int Age
+    {
+        get;
+    }
+}
+
+public class Person : ILivingCreature, IPerson
+{
+    public string Name
+    {
+        get;
+    }
+    public int Age
+    {
+        get; set;
+    }
+    public string Job
+    {
+        get; set;
+    }
+
+    public Person(string name, int age, string job)
+    {
+        Name = name;
+        Age = age;
+        Job = job;
+    }
+}
+
+public class Pet : ILivingCreature
+{
+    public string Name
+    {
+        get;
+    }
+    public int Age
+    {
+        get;
+    }
+}
