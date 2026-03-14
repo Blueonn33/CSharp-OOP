f5aa31a7ab424eddc0a1dd964572a27306549390
Martin Marinov <m.marinov1733@gmail.com>
2026-03-14 20:16:22 +0200

    stream progress

---
+++ Solid/P01.Stream_Progress/IStreamable.cs
@@ -0,0 +1,16 @@
+﻿namespace P01.Stream_Progress
+{
+    public interface IStreamable
+    {
+
+        public int Length
+        {
+            get;
+        }
+
+        public int BytesSent
+        {
+            get;
+        }
+    }
+}
---
+++ Solid/P01.Stream_Progress/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var progress = new StreamProgressInfo(new Music("John Secada", "Just another day", 10, 5));
+            var progress2 = new StreamProgressInfo(new File("Knigga", 10, 5));
+        }
+    }
+}
