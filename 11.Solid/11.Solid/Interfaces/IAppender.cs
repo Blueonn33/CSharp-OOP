05091d16091dfe0f46c8822070d54e914ce3ff24
Martin Marinov <m.marinov1733@gmail.com>
2026-03-14 22:37:06 +0200

    Add IAppender logging interface
    Introduce IAppender interface to define the contract for log appenders. It includes a configurable ReportLevel, a read-only MessagesAppended counter, and an Append(date, reportLevel, message) method. Added to the Interfaces folder as part of the logging components.

---
+++ 11.Solid/11.Solid/Interfaces/IAppender.cs
@@ -0,0 +1,19 @@
+﻿using _11.Solid.Enums;
+
+namespace _11.Solid.Interfaces
+{
+    public interface IAppender
+    {
+        ReportLevel ReportLevel
+        {
+            get; set;
+        }
+
+        int MessagesAppended
+        {
+            get;
+        }
+
+        void Append(string date, ReportLevel reportLevel, string message);
+    }
+}
