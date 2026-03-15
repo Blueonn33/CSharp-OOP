eadcd37c10a738816c72b01fa6a8cf68f3847f65
Martin Marinov <m.marinov1733@gmail.com>
2026-03-15 11:15:25 +0200

    file appender

--- 11.Solid/11.Solid/Appenders/FileAppender.cs
+++ 11.Solid/11.Solid/Appenders/FileAppender.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using _11.Solid.Enums;
+using _11.Solid.Interfaces;
 
 namespace _11.Solid.Appenders
 {
-    public class FileAppender
+    public class FileAppender : IAppender
     {
+        private readonly ILayout _layout;
+        private readonly ILogFile _logFile;
+
+        public ILogFile File
+        {
+            get;
+        }
+
+        public FileAppender(ILayout layout, ILogFile logFile)
+        {
+            _layout = layout;
+            _logFile = logFile;
+        }
+
+        public ReportLevel ReportLevel
+        {
+            get;
+            set;
+        }
+
+        public int MessagesAppended
+        {
+            get;
+            private set;
+        }
+
+        public void Append(string date, ReportLevel reportLevel, string message)
+        {
+            if (reportLevel >= ReportLevel)
+            {
+                string formattedMessage = _layout.Format(date, reportLevel, message);
+
+                _logFile.Write(formattedMessage);
+
+                MessagesAppended++;
+            }
+        }
     }
 }
