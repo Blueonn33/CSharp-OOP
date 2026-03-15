3493eac4c3c5f0c02d06158224c5fe5554865ebb
Martin Marinov <m.marinov1733@gmail.com>
2026-03-15 11:19:09 +0200

    appender in program

--- 11.Solid/11.Solid/Program.cs
+++ 11.Solid/11.Solid/Program.cs
@@ -2,16 +2,17 @@
 using _11.Solid.Core;
 using _11.Solid.Interfaces;
 using _11.Solid.Layouts;
+using _11.Solid.Services;
 
 public class Program
 {
     static void Main()
     {
-        ILayout layout = new SimpleLayout();
-        IAppender consoleAppender = new ConsoleAppender(layout);
+        ILayout layout = new XmlLayout();
+        IAppender appender = new FileAppender(layout, new LogFile());
         //consoleAppender.ReportLevel = ReportLevel.Error;
 
-        ILogger logger = new Logger(consoleAppender);
+        ILogger logger = new Logger(appender);
 
         logger.Error("3/26/2015 2:09:11 PM", "Error parsing JSON.");
         logger.Info("3/26/2015 2:09:11 PM", "User Martin has successfully registered.");
