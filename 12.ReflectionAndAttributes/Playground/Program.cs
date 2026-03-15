d57d8728ee6f13b187c79d2ec43227db79dcff1d
Martin Marinov <m.marinov1733@gmail.com>
2026-03-15 14:18:03 +0200

    types

--- 12.ReflectionAndAttributes/Playground/Program.cs
+++ 12.ReflectionAndAttributes/Playground/Program.cs
@@ -7,5 +7,10 @@ public class Program
         var catType = typeof(Cat);
 
         Console.WriteLine(catType.FullName);
+        Console.WriteLine(catType.IsAbstract);
+
+        var types = new List<Type>();
+        types.Add(catType);
+        types.Add(typeof(Animal));
     }
 }
