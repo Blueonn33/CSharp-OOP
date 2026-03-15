5bfc5f7c8faba074062420698df6e1966180b578
Martin Marinov <m.marinov1733@gmail.com>
2026-03-15 16:08:35 +0200

    stealer

---
+++ 12.ReflectionAndAttributes/Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldNames)
+        {
+            Console.WriteLine($"Class under investigation: {className}");
+
+            var type = Type.GetType(className);
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            var searchedFields = fields
+                .Where(f => fieldNames.Contains(f.Name))
+                .ToList();
+
+            var instance = Activator.CreateInstance(type);
+            var result = new StringBuilder();
+
+            foreach (var field in fields)
+            {
+                var value = field.GetValue(instance);
+                result.AppendLine($"{field.Name} = {value}");
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ 12.ReflectionAndAttributes/Stealer/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var spy = new Spy();
+
+            var result = spy.StealFieldInfo("Stealer.Hacker", "username", "password"
+            );
+
+            Console.WriteLine(result);
+        }
+    }
+}
