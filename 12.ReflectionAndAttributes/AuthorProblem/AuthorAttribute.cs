ccaed50e0d1f3f7ac1f0ff6766a5baab1fca22b8
Martin Marinov <m.marinov1733@gmail.com>
2026-03-15 17:30:26 +0200

    author

---
+++ 12.ReflectionAndAttributes/AuthorProblem/AuthorAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthorProblem
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    public class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+        public string Name
+        {
+            get; set;
+        }
+    }
+}
