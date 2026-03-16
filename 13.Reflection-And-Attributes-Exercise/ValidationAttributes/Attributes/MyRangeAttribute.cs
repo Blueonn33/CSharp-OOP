0e839566bf912ba4718f71e4387fbe607671d4fb
Martin Marinov <m.marinov1733@gmail.com>
2026-03-16 13:11:46 +0200

    validation classes

---
+++ 13.Reflection-And-Attributes-Exercise/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        public override bool IsValid(object obj)
+        {
+            
+        }
+    }
+}
