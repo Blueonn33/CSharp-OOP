fdca0d1550619b4b763e16acaed9357c8dd1acb9
Martin Marinov <m.marinov1733@gmail.com>
2026-03-16 11:56:39 +0200

    engine

--- 13.Reflection-And-Attributes-Exercise/CommandPattern/StartUp.cs
+++ 13.Reflection-And-Attributes-Exercise/CommandPattern/StartUp.cs
@@ -1,12 +1,14 @@
-﻿namespace CommandPattern
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            //ICommandInterpreter command = new CommandInterpreter();
-            //IEngine engine = new Engine(command);
-            //engine.Run();
+            ICommandInterpreter commandInterpreter = new CommandInterpreter();
+            IEngine engine = new Engine(commandInterpreter);
+            engine.Run();
         }
     }
 }
