b07591e3920031cf142210875e7733e487936739
Martin Marinov <m.marinov1733@gmail.com>
2026-03-16 13:21:22 +0200

    person

---
+++ 13.Reflection-And-Attributes-Exercise/ValidationAttributes/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace ValidationAttributes.Models
+{
+    public class Person
+    {
+        public Person(string fullName, int age)
+        {
+            FullName = fullName;
+            Age = age;
+        }
+
+        public string FullName
+        {
+            get; private set;
+        }
+        public int Age
+        {
+            get; private set;
+        }
+    }
+}
--- 13.Reflection-And-Attributes-Exercise/ValidationAttributes/StartUp.cs
+++ 13.Reflection-And-Attributes-Exercise/ValidationAttributes/StartUp.cs
@@ -1,14 +1,16 @@
-﻿namespace ValidationAttributes
+﻿using ValidationAttributes.Models;
+
+namespace ValidationAttributes
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            //var person = new Person
-            // (
-            //     null,
-            //     -1
-            // );
+            var person = new Person
+             (
+                 null,
+                 -1
+             );
 
             //bool isValidEntity = Validator.IsValid(person);
 
