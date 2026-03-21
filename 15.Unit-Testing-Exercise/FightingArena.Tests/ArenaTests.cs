848af8da30d12b74b5c0a4c565f784eea0cac7d0
Martin Marinov <m.marinov1733@gmail.com>
2026-03-22 00:03:56 +0200

    fighting arena

--- 15.Unit-Testing-Exercise/FightingArena.Tests/ArenaTests.cs
+++ 15.Unit-Testing-Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,76 @@
-﻿namespace FightingArena.Tests
-{
-    using NUnit.Framework;
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
 
-    [TestFixture]
+namespace FightingArena.Tests
+{
     public class ArenaTests
     {
+        private Arena arena;
 
+        [SetUp]
+        public void Setup()
+        {
+            arena = new Arena();
+        }
+
+        [Test]
+        public void Constructor_ShouldInitializeCollection()
+        {
+            Assert.IsNotNull(arena.Warriors);
+            Assert.AreEqual(0, arena.Count);
+        }
+
+        [Test]
+        public void Enroll_ShouldAddWarrior()
+        {
+            Warrior w = new Warrior("A", 10, 100);
+
+            arena.Enroll(w);
+
+            Assert.AreEqual(1, arena.Count);
+            Assert.IsTrue(arena.Warriors.Any(x => x.Name == "A"));
+        }
+
+        [Test]
+        public void Enroll_ShouldThrow_WhenWarriorAlreadyExists()
+        {
+            Warrior w = new Warrior("A", 10, 100);
+
+            arena.Enroll(w);
+
+            Assert.Throws<InvalidOperationException>(() => arena.Enroll(new Warrior("A", 20, 200)));
+        }
+
+        [Test]
+        public void Fight_ShouldThrow_WhenAttackerMissing()
+        {
+            arena.Enroll(new Warrior("B", 20, 100));
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("A", "B"));
+        }
+
+        [Test]
+        public void Fight_ShouldThrow_WhenDefenderMissing()
+        {
+            arena.Enroll(new Warrior("A", 20, 100));
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("A", "B"));
+        }
+
+        [Test]
+        public void Fight_ShouldReduceHP_OfBothWarriors()
+        {
+            Warrior attacker = new Warrior("A", 30, 100);
+            Warrior defender = new Warrior("B", 20, 100);
+
+            arena.Enroll(attacker);
+            arena.Enroll(defender);
+
+            arena.Fight("A", "B");
+
+            Assert.AreEqual(80, attacker.HP);
+            Assert.AreEqual(70, defender.HP);
+        }
     }
 }
--- 15.Unit-Testing-Exercise/FightingArena.Tests/WarriorTests.cs
+++ 15.Unit-Testing-Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,92 @@
+using NUnit.Framework;
+using System;
+
 namespace FightingArena.Tests
 {
-    using NUnit.Framework;
-
-    [TestFixture]
     public class WarriorTests
     {
+        [Test]
+        public void Constructor_ShouldInitializeCorrectly()
+        {
+            Warrior w = new Warrior("Gosho", 50, 100);
 
+            Assert.AreEqual("Gosho", w.Name);
+            Assert.AreEqual(50, w.Damage);
+            Assert.AreEqual(100, w.HP);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void Name_ShouldThrow_WhenInvalid(string name)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, 10, 50));
+        }
+
+        [TestCase(0)]
+        [TestCase(-10)]
+        public void Damage_ShouldThrow_WhenZeroOrNegative(int dmg)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Test", dmg, 50));
+        }
+
+        [TestCase(-1)]
+        [TestCase(-100)]
+        public void HP_ShouldThrow_WhenNegative(int hp)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Test", 10, hp));
+        }
+
+        [TestCase(29)]
+        [TestCase(0)]
+        public void Attack_ShouldThrow_WhenAttackerHPTooLow(int hp)
+        {
+            Warrior attacker = new Warrior("A", 50, hp);
+            Warrior defender = new Warrior("B", 40, 100);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(defender));
+        }
+
+        [TestCase(29)]
+        [TestCase(10)]
+        public void Attack_ShouldThrow_WhenDefenderHPTooLow(int hp)
+        {
+            Warrior attacker = new Warrior("A", 50, 100);
+            Warrior defender = new Warrior("B", 40, hp);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(defender));
+        }
+
+        [Test]
+        public void Attack_ShouldThrow_WhenAttackingStrongerEnemy()
+        {
+            Warrior attacker = new Warrior("A", 50, 50);
+            Warrior defender = new Warrior("B", 60, 100);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(defender));
+        }
+
+        [Test]
+        public void Attack_ShouldReduceHP_Correctly()
+        {
+            Warrior attacker = new Warrior("A", 50, 100);
+            Warrior defender = new Warrior("B", 40, 100);
+
+            attacker.Attack(defender);
+
+            Assert.AreEqual(60, attacker.HP);
+            Assert.AreEqual(50, defender.HP);
+        }
+
+        [Test]
+        public void Attack_ShouldSetDefenderHPToZero_WhenDamageIsGreater()
+        {
+            Warrior attacker = new Warrior("A", 200, 100);
+            Warrior defender = new Warrior("B", 40, 150);
+
+            attacker.Attack(defender);
+
+            Assert.AreEqual(0, defender.HP);
+        }
     }
 }
