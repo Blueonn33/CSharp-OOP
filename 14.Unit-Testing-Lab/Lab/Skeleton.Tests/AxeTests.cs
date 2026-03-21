ff4acd45d804acc8f6700eddd85246a946736ff3
Martin Marinov <m.marinov1733@gmail.com>
2026-03-21 13:27:34 +0200

    axe test

--- 14.Unit-Testing-Lab/Lab/Skeleton.Tests/AxeTests.cs
+++ 14.Unit-Testing-Lab/Lab/Skeleton.Tests/AxeTests.cs
@@ -6,9 +6,18 @@ namespace Skeleton.Tests
     public class AxeTests
     {
         [Test]
-        public void Test1()
+        public void AxeShouldLoseDurabilityAfterEachAttack()
         {
+            // Arrange
+            var axe = new Axe(10, 20);
+            var dummy = new Dummy(100, 200);
 
+            // Act
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+
+            // Assert
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(18));
         }
     }
 }
