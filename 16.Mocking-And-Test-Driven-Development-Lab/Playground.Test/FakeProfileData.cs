8a903583fe68d36b1db2104fabc4ede053e5cc3a
Martin Marinov <m.marinov1733@gmail.com>
2026-03-22 21:33:37 +0200

    fake data

---
+++ 16.Mocking-And-Test-Driven-Development-Lab/Playground.Test/FakeProfileData.cs
@@ -0,0 +1,17 @@
+﻿namespace Playground.Test
+{
+    public class FakeProfileData : IProfileData
+    {
+        public List<Profile> All()
+        {
+            // Fake data for testing purposes
+
+            return new List<Profile>
+            {
+                new Profile { Age = 20 },
+                new Profile { Age = 22 },
+                new Profile { Age = 10 },
+            };
+        }
+    }
+}
