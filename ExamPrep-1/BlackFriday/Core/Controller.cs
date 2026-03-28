cc5e077548c2655a2d2595e9cb26d7e1e30c0ebd
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 21:15:52 +0200

    controller

---
+++ ExamPrep-1/BlackFriday/Core/Controller.cs
@@ -0,0 +1,37 @@
+﻿using BlackFriday.Core.Contracts;
+
+namespace BlackFriday.Core
+{
+    public class Controller : IController
+    {
+        public string RegisterUser(string userName, string email, bool hasDataAccess)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddProduct(string productType, string productName, string userName, double basePrice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UpdateProductPrice(string productName, string userName, double newPriceValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RefreshSalesList(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PurchaseProduct(string userName, string productName, bool blackFridayFlag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ApplicationReport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
