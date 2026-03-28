401567ae95448bc0fc6df3ecdd784da35506925d
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 19:28:56 +0200

    user

--- ExamPrep-1/BlackFriday/Models/User.cs
+++ ExamPrep-1/BlackFriday/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using BlackFriday.Models.Contracts;
+using BlackFriday.Utilities.Messages;
 
 namespace BlackFriday.Models
 {
@@ -9,7 +10,7 @@ namespace BlackFriday.Models
             get;
         }
 
-        public abstract bool HasDataAccess
+        public bool HasDataAccess
         {
             get;
         }
@@ -17,6 +18,35 @@ namespace BlackFriday.Models
         public string Email
         {
             get;
+        }
+
+        protected User(string userName, string email, bool hasDataAccess)
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                throw new ArgumentException(ExceptionMessages.UserNameRequired);
+            }
+
+            if (string.IsNullOrEmpty(email))
+            {
+                if (hasDataAccess)
+                {
+                    email = "hidden";
+                }
+                else
+                {
+                    throw new ArgumentException(ExceptionMessages.EmailRequired);
+                }
+            }
+
+            UserName = userName;
+            Email = email;
+            HasDataAccess = hasDataAccess;
+        }
+
+        public override string ToString()
+        {
+            return $"{UserName} - Status: {GetType().Name}, Contact Info: {Email}";
         }
     }
 }
