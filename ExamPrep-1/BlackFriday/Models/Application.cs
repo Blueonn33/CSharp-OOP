af3e0f465e52c76aac34ae5cc46b704523e32609
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 19:52:08 +0200

    application

---
+++ ExamPrep-1/BlackFriday/Models/Application.cs
@@ -0,0 +1,25 @@
+﻿using BlackFriday.Models.Contracts;
+using BlackFriday.Repositories;
+using BlackFriday.Repositories.Contracts;
+
+namespace BlackFriday.Models
+{
+    public class Application : IApplication
+    {
+        public IRepository<IProduct> Products
+        {
+            get;
+        }
+
+        public IRepository<IUser> Users
+        {
+            get;
+        }
+
+        public Application()
+        {
+            Products = new ProductRepository();
+            Users = new UserRepository();
+        }
+    }
+}
