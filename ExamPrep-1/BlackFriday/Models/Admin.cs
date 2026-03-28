27e093657a4ade1c8f1c684c98bd3655e868c3bb
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 19:31:25 +0200

    admin

---
+++ ExamPrep-1/BlackFriday/Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace BlackFriday.Models
+{
+    public class Admin : User
+    {
+        public Admin(string userName, string email, bool hasDataAccess) : base(userName, email, hasDataAccess: true)
+        {
+        }
+    }
+}
