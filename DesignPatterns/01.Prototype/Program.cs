9597678e9fc5690b206c8fcfa9bad3354aae09c9
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 13:31:19 +0200

    student and laptop

--- DesignPatterns/01.Prototype/Program.cs
+++ DesignPatterns/01.Prototype/Program.cs
@@ -1,10 +1,51 @@
 ﻿namespace _01.Prototype
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Student
+    {
+        public Student(int age, string name, Laptop laptop)
+        {
+            Age = age;
+            Name = name;
+            Laptop = laptop;
+        }
+
+        public int Age
+        {
+            get; private set;
+        }
+        public string Name
+        {
+            get; private set;
+        }
+        public Laptop Laptop
+        {
+            get; set;
+        }
+    }
+
+    public class Laptop
+    {
+        public Laptop(string brand, decimal price)
+        {
+            Brand = brand;
+            Price = price;
+        }
+
+        public string Brand
+        {
+            get; private set;
+        }
+        public decimal Price
+        {
+            get; set;
         }
     }
 }
