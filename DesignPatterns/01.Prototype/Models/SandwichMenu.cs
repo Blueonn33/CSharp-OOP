be1da50e7d96a5dd002322e33029d3859e1bd6be
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 14:04:58 +0200

    sandwich menu

---
+++ DesignPatterns/01.Prototype/Models/SandwichMenu.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.Prototype.Models
+{
+    public class SandwichMenu
+    {
+        private readonly IDictionary<string, SandwichPrototype> sandwiches;
+
+        public SandwichMenu()
+        {
+            sandwiches = new Dictionary<string, SandwichPrototype>();
+        }
+
+        public SandwichPrototype this[string name]
+        {
+            get
+            {
+                if (!sandwiches.ContainsKey(name))
+                {
+                    throw new ArgumentException($"Sandwich with name {name} does not exist.");
+                }
+
+                return sandwiches[name];
+            }
+            set
+            {
+                sandwiches[name] = value;
+            }
+        }
+    }
+}
