28fdcffa1fc0b7bb387e296a138a80325ad3e067
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 18:11:48 +0200

    product

--- ExamPrep-1/BlackFriday/Models/Product.cs
+++ ExamPrep-1/BlackFriday/Models/Product.cs
@@ -13,19 +13,27 @@ namespace BlackFriday.Models
         }
         public double BasePrice
         {
-            get => this._basePrice;
-            set
+            get => _basePrice;
+            private set
             {
-                this._basePrice = value;
+                // If the property is mutable, prefer validations within the `set` accessor
+
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.ProductPriceConstraints);
+                }
+
+                _basePrice = value;
             }
         }
-        public double BlackFridayPrice
+        public virtual double BlackFridayPrice
         {
             get;
         }
         public bool IsSold
         {
             get;
+            private set;
         }
 
         protected Product(string productName, double basePrice)
@@ -44,12 +52,17 @@ namespace BlackFriday.Models
 
         public void UpdatePrice(double newPriceValue)
         {
-            throw new NotImplementedException();
+            BasePrice = newPriceValue;
         }
 
         public void ToggleStatus()
         {
-            throw new NotImplementedException();
+            IsSold = !IsSold;
+        }
+
+        public override string ToString()
+        {
+            return $"Product: {ProductName}, Price: {BasePrice:F2}, You Save: {(BasePrice - BlackFridayPrice):F2}";
         }
     }
 }
