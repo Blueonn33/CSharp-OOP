8fc25a4bfac94e13de3f449512414e7d30717754
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 18:04:23 +0200

    validations

--- ExamPrep-1/BlackFriday/Models/Product.cs
+++ ExamPrep-1/BlackFriday/Models/Product.cs
@@ -1,16 +1,23 @@
 ﻿using BlackFriday.Models.Contracts;
+using BlackFriday.Utilities.Messages;
 
 namespace BlackFriday.Models
 {
     public abstract class Product : IProduct
     {
+        private double _basePrice;
+
         public string ProductName
         {
             get;
         }
         public double BasePrice
         {
-            get;
+            get => this._basePrice;
+            set
+            {
+                this._basePrice = value;
+            }
         }
         public double BlackFridayPrice
         {
@@ -23,6 +30,14 @@ namespace BlackFriday.Models
 
         protected Product(string productName, double basePrice)
         {
+            // I prefer validations directly within the constructor.
+            // This idea is applicable for immutable properties only.
+
+            if (string.IsNullOrWhiteSpace(productName))
+            {
+                throw new ArgumentException(ExceptionMessages.ProductNameRequired);
+            }
+
             ProductName = productName;
             BasePrice = basePrice;
         }
