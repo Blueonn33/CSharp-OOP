9d65c1ae53894540482f0885389fedd461b5dd0d
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 18:26:04 +0200

    user

---
+++ ExamPrep-1/BlackFriday/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using BlackFriday.Models.Contracts;
+
+namespace BlackFriday.Models
+{
+    public abstract class User : IUser
+    {
+        public string UserName
+        {
+            get;
+        }
+
+        public abstract bool HasDataAccess
+        {
+            get;
+        }
+
+        public string Email
+        {
+            get;
+        }
+    }
+}
