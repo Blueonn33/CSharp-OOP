4d38266d433d67c5d89b36f238a5fc1d2b6d50d8
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 16:32:48 +0200

    program

--- DesignPatterns/02.Composite/Program.cs
+++ DesignPatterns/02.Composite/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace _02.Composite
+﻿using _02.Composite.Models;
+
+namespace _02.Composite
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            SingleGift phone = new SingleGift("Phone", 257);
+            phone.CalculateTotalPrice();
+
+            Console.WriteLine();
+
+            CompositeGift rootBox = new CompositeGift("Root box", 0);
+
+            SingleGift truck = new SingleGift("Truck", 50);
+            SingleGift plane = new SingleGift("Plane", 100);
+
+            rootBox.AddGift(truck);
+            rootBox.AddGift(plane);
+
+            rootBox.CalculateTotalPrice();
         }
     }
 }
