c773b815b51843016cb119eab06d001be84cdaaf
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 16:55:23 +0200

    wheat

---
+++ DesignPatterns/03.TemplatePattern/WholeWheat.cs
@@ -0,0 +1,15 @@
+﻿namespace _03.TemplatePattern
+{
+    public class WholeWheat : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine($"Gathering ingredients for Whole wheat");
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine($"Baking for 33 minutes");
+        }
+    }
+}
