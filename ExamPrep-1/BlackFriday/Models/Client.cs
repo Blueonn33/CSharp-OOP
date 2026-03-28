66caac6b4a68658e49203d332d1cf76fa82ebfae
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 19:30:30 +0200

    client

---
+++ ExamPrep-1/BlackFriday/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace BlackFriday.Models
+{
+    public class Client : User
+    {
+        public Client(string userName, string email, bool hasDataAccess) : base(userName, email, hasDataAccess)
+        {
+        }
+
+
+    }
+}
