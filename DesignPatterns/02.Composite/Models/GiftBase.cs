7859e00e2ad9e2d621c019d302c6c2570168b5aa
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 16:17:18 +0200

    props

--- DesignPatterns/02.Composite/Models/GiftBase.cs
+++ DesignPatterns/02.Composite/Models/GiftBase.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _02.Composite.Models
+﻿namespace _02.Composite.Models
 {
     public abstract class GiftBase
     {
+        private string name;
+        private decimal price;
+
+        public GiftBase(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        protected string Name
+        {
+            get => name;
+            private set => name = value;
+        }
+        protected decimal Price
+        {
+            get => price;
+            private set => price = value;
+        }
     }
 }
