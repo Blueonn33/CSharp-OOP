690792a08acd07108b5e566d0e4e5abf0a54ea14
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 19:42:36 +0200

    user repo

---
+++ ExamPrep-1/BlackFriday/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using BlackFriday.Models.Contracts;
+using BlackFriday.Repositories.Contracts;
+using System.Collections.ObjectModel;
+
+namespace BlackFriday.Repositories
+{
+    public class UserRepository : IRepository<IUser>
+    {
+        private readonly List<IUser> models;
+        public IReadOnlyCollection<IUser> Models
+        {
+            get;
+        }
+
+        public UserRepository()
+        {
+            models = new List<IUser>();
+            Models = new ReadOnlyCollection<IUser>(models);
+        }
+
+        public void AddNew(IUser model)
+        {
+            models.Add(model);
+        }
+
+        public bool Exists(string name)
+        {
+            return models.Any(p => p.UserName == name);
+        }
+
+        public IUser GetByName(string name)
+        {
+            return models.FirstOrDefault(x => x.UserName == name);
+        }
+    }
+}
