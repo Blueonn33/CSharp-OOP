87659543116bc8cb4625fdfae881e898a9b97776
Martin Marinov <m.marinov1733@gmail.com>
2026-03-28 17:58:15 +0200

    product

---
+++ ExamPrep-1/BlackFriday/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using BlackFriday.Models.Contracts;
+
+namespace BlackFriday.Models
+{
+    public abstract class Product : IProduct
+    {
+        public string ProductName
+        {
+            get;
+        }
+        public double BasePrice
+        {
+            get;
+        }
+        public double BlackFridayPrice
+        {
+            get;
+        }
+        public bool IsSold
+        {
+            get;
+        }
+
+        public void UpdatePrice(double newPriceValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToggleStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
