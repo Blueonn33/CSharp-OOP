dfe14a9ac1590b7e471a7cb6d10eb8fc01d5f748
Martin Marinov <m.marinov1733@gmail.com>
2026-03-29 16:53:07 +0300

    add customer

--- ExamPrep-2/CarDealership/Core/Controller.cs
+++ ExamPrep-2/CarDealership/Core/Controller.cs
@@ -1,12 +1,38 @@
 ﻿using CarDealership.Core.Contracts;
+using CarDealership.Models;
+using CarDealership.Models.Contracts;
+using CarDealership.Utilities.Messages;
 
 namespace CarDealership.Core
 {
     public class Controller : IController
     {
+        private readonly IDealership dealership;
+
+        public Controller()
+        {
+            dealership = new Dealership();
+        }
+
         public string AddCustomer(string customerTypeName, string customerName)
         {
-            throw new NotImplementedException();
+            if (customerTypeName != nameof(IndividualClient) && customerTypeName != nameof(LegalEntityCustomer))
+            {
+                return string.Format(OutputMessages.InvalidType, customerTypeName);
+            }
+
+            if (dealership.Customers.Exists(customerName))
+            {
+                return string.Format(OutputMessages.CustomerAlreadyAdded, customerName);
+            }
+
+            ICustomer customer = customerName == nameof(IndividualClient)
+                ? new IndividualClient(customerName)
+                : new LegalEntityCustomer(customerName);
+
+            dealership.Customers.Add(customer);
+
+            return string.Format(OutputMessages.CustomerAddedSuccessfully, customerName);
         }
 
         public string AddVehicle(string vehicleTypeName, string model, double price)
