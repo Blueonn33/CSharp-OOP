7c8c271f0cd0e0b21381ca6b7f06006cc4925ade
Martin Marinov <m.marinov1733@gmail.com>
2026-03-29 14:51:03 +0300

    customers

---
+++ ExamPrep-2/CarDealership/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using CarDealership.Models.Contracts;
+
+namespace CarDealership.Models
+{
+    public abstract class Customer : ICustomer
+    {
+        public string Name => throw new NotImplementedException();
+
+        public IReadOnlyCollection<string> Purchases => throw new NotImplementedException();
+
+        public void BuyVehicle(string vehicleModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
