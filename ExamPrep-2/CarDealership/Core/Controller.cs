3fea62e65ae3141f53557e447bdcb2453630e723
Martin Marinov <m.marinov1733@gmail.com>
2026-03-29 14:44:48 +0300

    controller

---
+++ ExamPrep-2/CarDealership/Core/Controller.cs
@@ -0,0 +1,32 @@
+﻿using CarDealership.Core.Contracts;
+
+namespace CarDealership.Core
+{
+    public class Controller : IController
+    {
+        public string AddCustomer(string customerTypeName, string customerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddVehicle(string vehicleTypeName, string model, double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CustomerReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PurchaseVehicle(string vehicleTypeName, string customerName, double budget)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SalesReport(string vehicleTypeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
