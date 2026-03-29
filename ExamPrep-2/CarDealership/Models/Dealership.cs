928229176b1488f15684f91f79e735699438f3c9
Martin Marinov <m.marinov1733@gmail.com>
2026-03-29 14:54:01 +0300

    dealer

---
+++ ExamPrep-2/CarDealership/Models/Dealership.cs
@@ -0,0 +1,17 @@
+﻿using CarDealership.Models.Contracts;
+using CarDealership.Repositories.Contracts;
+
+namespace CarDealership.Models
+{
+    public class Dealership : IDealership
+    {
+        public IRepository<IVehicle> Vehicles
+        {
+            get;
+        }
+        public IRepository<ICustomer> Customers
+        {
+            get;
+        }
+    }
+}
