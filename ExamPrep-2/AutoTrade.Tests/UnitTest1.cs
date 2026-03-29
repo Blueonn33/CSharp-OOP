ce24f757d5263e144e694aae04ca9281c9caa337
Martin Marinov <m.marinov1733@gmail.com>
2026-03-29 14:09:20 +0300

    exam prep

---
+++ ExamPrep-2/AutoTrade.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AutoTrade.Tests
+{
+    [TestFixture]
+    public class DealerShopTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
