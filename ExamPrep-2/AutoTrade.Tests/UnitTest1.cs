7a3119d3a9c378c4b1c524cf1ed06bb7b4307fbd
Martin Marinov <m.marinov1733@gmail.com>
2026-03-29 18:26:12 +0300

    zero capacity

--- ExamPrep-2/AutoTrade.Tests/UnitTest1.cs
+++ ExamPrep-2/AutoTrade.Tests/UnitTest1.cs
@@ -11,6 +11,17 @@ namespace AutoTrade.Tests
             DealerShop shop = new DealerShop(10);
 
             Assert.AreEqual(expectedCapacity, shop.Capacity);
+            Assert.IsNotNull(shop.Vehicles);
+            Assert.IsEmpty(shop.Vehicles);
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void Constructor_ShouldThrowException_WhenCapacityIsZeroOrNegative(int capacity)
+        {
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => new DealerShop(capacity));
+
+            Assert.AreEqual("Capacity must be a positive value.", ex.Message);
         }
     }
 }
