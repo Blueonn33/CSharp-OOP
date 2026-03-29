3bc926f57142498d6c27cfac7aad30e2e16f7e19
Martin Marinov <m.marinov1733@gmail.com>
2026-03-29 14:52:54 +0300

    repositories

---
+++ ExamPrep-2/CarDealership/Repositories/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿using CarDealership.Models.Contracts;
+using CarDealership.Repositories.Contracts;
+
+namespace CarDealership.Repositories
+{
+    public class CustomerRepository : IRepository<ICustomer>
+    {
+        public IReadOnlyCollection<ICustomer> Models
+        {
+            get;
+        }
+        public void Add(ICustomer model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICustomer Get(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExamPrep-2/CarDealership/Repositories/VehicleRepository.cs
@@ -0,0 +1,30 @@
+﻿using CarDealership.Models.Contracts;
+using CarDealership.Repositories.Contracts;
+
+namespace CarDealership.Repositories
+{
+    public class VehicleRepository : IRepository<IVehicle>
+    {
+        public IReadOnlyCollection<IVehicle> Models => throw new NotImplementedException();
+
+        public void Add(IVehicle model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVehicle Get(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
