8e2d0bcee80d56fa47b96c2ebae9e720190a3e4c
Martin Marinov <m.marinov1733@gmail.com>
2026-04-03 12:21:15 +0300

    ibuyer

---
+++ 06.Interfaces-And-Abstraction-Exercise/BorderControl/Models/Interfaces/IBuyer.cs
@@ -0,0 +1,11 @@
+﻿namespace BorderControl.Models.Interfaces
+{
+    public interface IBuyer
+    {
+        public int Food
+        {
+            get; set;
+        }
+        void BuyFood();
+    }
+}
