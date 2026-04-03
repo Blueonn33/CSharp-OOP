12c4d7ef8917b409317b9e04c050e737cea27433
Martin Marinov <m.marinov1733@gmail.com>
2026-04-03 22:40:06 +0300

    mammals

--- 08.Polymorphism-Exercise/WildFarm/MammalModels/Dog.cs
+++ 08.Polymorphism-Exercise/WildFarm/MammalModels/Dog.cs
@@ -1,12 +1,22 @@
 ﻿using WildFarm.Abstract;
+using WildFarm.FoodModels;
 
 namespace WildFarm.MammalModels
 {
     public class Dog : Mammal
     {
-        public override string ProduceSound()
+        public Dog(string name, double weight, string region)
+            : base(name, weight, region) { }
+
+        public override string ProduceSound() => "Woof!";
+
+        public override void Eat(Food food)
         {
-            return "Woof!";
+            if (food is not Meat)
+                throw new InvalidOperationException($"{this.GetType().Name} does not eat {food.GetType().Name}!");
+
+            this.Weight += food.Quantity * 0.40;
+            this.FoodEaten += food.Quantity;
         }
     }
 }
--- 08.Polymorphism-Exercise/WildFarm/MammalModels/Mouse.cs
+++ 08.Polymorphism-Exercise/WildFarm/MammalModels/Mouse.cs
@@ -1,12 +1,22 @@
 ﻿using WildFarm.Abstract;
+using WildFarm.FoodModels;
 
 namespace WildFarm.MammalModels
 {
     public class Mouse : Mammal
     {
-        public override string ProduceSound()
+        public Mouse(string name, double weight, string region)
+            : base(name, weight, region) { }
+
+        public override string ProduceSound() => "Squeak";
+
+        public override void Eat(Food food)
         {
-            return "Squeak";
+            if (food is not Vegetable && food is not Fruit)
+                throw new InvalidOperationException($"{this.GetType().Name} does not eat {food.GetType().Name}!");
+
+            this.Weight += food.Quantity * 0.10;
+            this.FoodEaten += food.Quantity;
         }
     }
 }
