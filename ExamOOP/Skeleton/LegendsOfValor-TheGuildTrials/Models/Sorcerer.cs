0c732803ce79bf3f755d593bda26476ce4ee8a32
Martin Marinov <m.marinov1733@gmail.com>
2026-04-04 09:40:33 +0300

    sorcerer

---
+++ ExamOOP/Skeleton/LegendsOfValor-TheGuildTrials/Models/Sorcerer.cs
@@ -0,0 +1,16 @@
+﻿namespace LegendsOfValor_TheGuildTrials.Models
+{
+    public class Sorcerer : Hero
+    {
+        public Sorcerer(string name, string runeMark) : base(name, runeMark, power: 40, mana: 120, stamina: 0)
+        {
+
+        }
+
+        public override void Train()
+        {
+            Power += 20;
+            Mana += 25;
+        }
+    }
+}
