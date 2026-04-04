a3cc0ffab607bf273ce833821a6d3f53e8755389
Martin Marinov <m.marinov1733@gmail.com>
2026-04-04 10:26:37 +0300

    add model

--- ExamOOP/Skeleton/LegendsOfValor-TheGuildTrials/Repositories/HeroRepository.cs
+++ ExamOOP/Skeleton/LegendsOfValor-TheGuildTrials/Repositories/HeroRepository.cs
@@ -1,6 +1,36 @@
-﻿namespace LegendsOfValor_TheGuildTrials.Repositories
+﻿using LegendsOfValor_TheGuildTrials.Models.Contracts;
+using LegendsOfValor_TheGuildTrials.Repositories.Contratcs;
+using System.Collections.ObjectModel;
+
+namespace LegendsOfValor_TheGuildTrials.Repositories
 {
-    public class HeroRepository
+    public class HeroRepository : IRepository<IHero>
     {
+        private readonly List<IHero> entities;
+        public IReadOnlyCollection<IHero> Entities
+        {
+            get;
+        }
+
+        public HeroRepository()
+        {
+            entities = new List<IHero>();
+            Entities = new ReadOnlyCollection<IHero>(entities);
+        }
+
+        public void AddModel(IHero entity)
+        {
+            entities.Add(entity);
+        }
+
+        public IReadOnlyCollection<IHero> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHero GetModel(string runeMarkOrGuildName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
