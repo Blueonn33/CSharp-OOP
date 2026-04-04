71d6bb4159b50f77d598cb512e0627f3a0524a49
Martin Marinov <m.marinov1733@gmail.com>
2026-04-04 09:42:00 +0300

    spellblade

---
+++ ExamOOP/Skeleton/LegendsOfValor-TheGuildTrials/Models/Spellblade.cs
@@ -0,0 +1,16 @@
+﻿namespace LegendsOfValor_TheGuildTrials.Models
+{
+    public class Spellblade : Hero
+    {
+        public Spellblade(string name, string runeMark) : base(name, runeMark, power: 50, mana: 60, stamina: 60)
+        {
+        }
+
+        public override void Train()
+        {
+            Power += 15;
+            Mana += 10;
+            Stamina += 10;
+        }
+    }
+}
